2b624883a290e043c70725c4c7dc16251f248c6c
engsondos <engsondosahmed1@gmail.com>
2023-05-20 22:29:28 +0300

    Add DataConnection

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require("mongoose")
+
+const app = express()
+app.use(express.json())
+
+
+const PORT =5000
+
+
+app.listen(PORT,()=>{
+    console.log("Listening...")
+})
+
+// index.js
+
+mongoose.connect("mongodb://127.0.0.1:27017/BookDb", () =>
+  console.log("Connected...")
+);
+
+dbNative = mongoose.connection.db;
+console.log(dbNative);
+
+
