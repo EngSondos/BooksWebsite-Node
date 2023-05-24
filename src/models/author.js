5873eb31cf96704573ce392496412ee162a4a5cc
Khaled-318 <khaledd.adel318@gmail.com>
2023-05-24 14:50:01 +0300

    Adding Author Schema Validation

--- src/models/author.js
+++ src/models/author.js
@@ -1,11 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const authorSchema = new mongoose.Schema ({
     photo: {type: String},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    dateOfBirth: {type: Date },
+    dateOfBirth: { type: Date },
 })
 
-const authorModel = mongoose.model("author", authorSchema)
-module.exports = authorModel  
+function authorValidation(authorSchema) {
+    const schema = Joi.object({
+            firstName: Joi.string() .required(),
+            lastName: Joi.string() .required(),
+            dateOfBirth: Joi.date(),
+    })
+    return schema.validate(authorSchema); 
+}
+const AuthorModel = mongoose.model("author", authorSchema);
+
+module.exports = { AuthorModel,  authorValidation};
