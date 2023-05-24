6367744b69068021f46890d74732da82af21815a
Sondos Ahmed <engsondosahmed1@gmail.com>
2023-05-24 13:40:41 +0300

    Edit in book model

--- src/models/book.js
+++ src/models/book.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
 
     rating : {type: Number,default: null},
-    
-      //userId
-      //userId : { type: mongoose.Schema.Types.ObjectId , ref:"user",require: true},
 
+      userId : { type: mongoose.Schema.Types.ObjectId , ref:"user",require: true},
+      review:{type:String}
     })
 
 const bookSchema = new mongoose.Schema({
@@ -15,7 +14,7 @@ title : {type: String , require: true},
 description : {type: String , require: true},
 image : {type: String , require: true},
 categoryId : { type: mongoose.Schema.Types.ObjectId , ref:"category",require: true},
-   //AuthorId : { type: mongoose.Schema.Types.ObjectId , ref:"author",require: true},
+   AuthorId : { type: mongoose.Schema.Types.ObjectId , ref:"author",require: true},
 
 review :[reviewSchema],
 statususers: 
