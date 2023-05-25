2ad2e97b7bbe77f9e0a360ebc6716dc5330ee83e
Sallyzahran <sallyzahran.sz@gmail.com>
2023-05-25 13:10:03 +0300

    categoryRoutes

---
+++ src/routes/category.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const categoryController = require('../controllers/category')
+
+
+
+router.post('/',categoryController.addCategory)
+router.get('/',categoryController.getCategories)
+router.get('/:id',categoryController.getCategory)
+router.put('/:id',categoryController.updateCategory)
+router.delete('/:id',categoryController.deleteCategory)
+
+module.exports= router
