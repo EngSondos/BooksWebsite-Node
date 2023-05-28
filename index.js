15dbf06a7a203fbc9dd4cd16fa4fb9ac3dce1bb1
Sondos Ahmed <engsondosahmed1@gmail.com>
2023-05-28 19:40:51 +0300

    Edit in index

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const mongoose = require("mongoose")
 const reviewRouter = require('./src/routes/review')
 const categoryRouter = require('./src/routes/category')
+const bookRouter = require('./src/routes/book')
+const {categoryModel} =require('./src/models/category')
+const {authorModel} =require('./src/models/author')
 
 
 const app = express()
 app.use(express.json())
+app.use('/assets', express.static('assets'));
 
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 const PORT =5000
 
 app.listen(PORT,()=>{
@@ -17,6 +28,8 @@ app.listen(PORT,()=>{
 
 app.use('/category',categoryRouter)
 app.use('/review',reviewRouter)
+app.use('/book',bookRouter)
+
 
 
 
