ae80617e1fd6a8c188e5c64d472c96a2a72a7c77
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-02 23:32:43 +0300

    add Auth Middleware

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const categoryController = require('../controllers/category')
+const auth = require('../../middelware/auth')
 
 
 
-router.post('/',categoryController.addCategory)
-router.get('/',categoryController.getCategories)
-router.get('/:id',categoryController.getCategory)
-router.put('/:id',categoryController.updateCategory)
-router.delete('/:id',categoryController.deleteCategory)
-router.get('/:id/book',categoryController.categoriesbooks)
+router.post('/',auth,categoryController.addCategory)
+router.get('/',auth,categoryController.getCategories)
+router.get('/:id',auth,categoryController.getCategory)
+router.put('/:id',auth,categoryController.updateCategory)
+router.delete('/:id',auth,categoryController.deleteCategory)
+router.get('/:id/book',auth,categoryController.categoriesbooks)
 
 module.exports= router
